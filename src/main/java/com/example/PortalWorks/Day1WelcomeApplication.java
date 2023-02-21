32eb6720652c94a9079c57b5ea2cf14ed0a0a979
dhanush001-k <727721euit028@skcet.ac.in>
2023-02-21 12:11:23 +0530

    First Commit

---
+++ src/main/java/com/example/PortalWorks/Day1WelcomeApplication.java
@@ -0,0 +1,13 @@
+package com.example.PortalWorks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day1WelcomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day1WelcomeApplication.class, args);
+	}
+
+}
